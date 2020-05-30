9f172a3f3fedc4b9d90509d0bd0dbd99fce7629c
Gayrat <gena68@inbox.ru>
2020-05-30 10:36:14 +0300

    final commit - JINO server

--- gcs.php
+++ gcs.php
@@ -13,14 +13,19 @@ function GUID()
     return sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535));
 }
 
+// if(! isset($_FILES)) {
+//     echo 'Файл не вложен в запрос';
+//     die;
+// }
+
 # Проверяем, загрузил ли пользователь файл
-if(isset($_FILES) && $_FILES['file']['error'] == 0) {
+if(isset($_FILES) &&  $_FILES['file']['error'] == 0) {
     $allowed = array('png', 'jpg', 'gif', 'jpeg','webp');
 
     $ext = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
 
     if(!in_array(strtolower($ext), $allowed)) {
-        echo 'Ваш файл не является изображением.';
+        echo 'Ваш файл не является изображением. ';
         die;
     }
 
@@ -34,7 +39,7 @@ if(isset($_FILES) && $_FILES['file']['error'] == 0) {
 
     $resize_files =  resizeImage($_FILES['file']['tmp_name'],$root_dir . $GCP_name . '-350.jpg',$root_dir . $GCP_name . '-1600.jpg') ;
 
-    $shared_URL = "http://ggvlasov.myjino.ru/resize/" ;
+    $shared_URL = "https://woodtoolsimg.ru/resize/" ;
 
     $ret_name_1600 = $shared_URL . $GCP_name . '-1600.jpg';
     $ret_name_350 = $shared_URL . $GCP_name . '-350.jpg';
